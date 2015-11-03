e68c08d78c7ef2aa592831b65d802d5893cf7892
konefes <konefes@gmail.com>
2015-11-03 15:36:20 +0000

    started registration cucumber tests

---
+++ features/step_definitions/vendor_registration_steps.rb
@@ -0,0 +1,20 @@
+Given /^I am on the contact info page$/ do
+    visit vendors_registration_path
+end
+
+When /I have filled out my information with:/ do |contact_info_table|
+    contact_info_table.hashes.each do |info_line|
+        current = page.find("##{info_line[:field]}")
+        if current.tag_name == 'select'
+            select info_line[:info], :from => info_line[:field]
+        elsif info_line[:field] == 'submit'
+            click_button info_line[:info]
+        else
+            fill_in info_line[:field], :with => info_line[:info] 
+        end
+    end
+    
+    
+    #click_button 'Submit Contact Information'
+    
+end
