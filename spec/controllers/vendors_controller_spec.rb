46a0f9f6959921eb6c3e3d14683564c18b80fffe
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-04 08:33:04 +0000

    added empty test skeletons for vendors controller

--- spec/controllers/vendors_controller_spec.rb
+++ spec/controllers/vendors_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe VendorsController, type: :controller do
-
+    describe "showing Vendor" do
+        it "should call the model method that lists all Vendors" do
+            pending
+        end
+    end
+    describe "entering Vendor Registration" do
+        it "should call the model method that creates User" do
+            pending
+        end
+        it "should call the model method that creates Vendor" do
+            pending
+        end
+    end
+    describe "entering Custom Booth Info" do
+        it "should call the model method that finds Vendor Id" do
+            pending
+        end
+    end
+    describe "entering Vendor Services" do
+         it "should call the model method that finds Vendor Id" do
+            pending
+        end
+    end
+    describe "viewing Vendor Summary" do
+        it "should call the model method that finds Vendor Id" do
+            pending
+        end
+        it "should call the model method that finds User Id" do
+            pending
+        end
+    end
+    describe "entering edit Vendor" do
+    end
+    describe "confirming Vendor Info" do
+    end
 end
