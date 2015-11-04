172ee4fcdf50bff1bf7f603a1b5c26d807033c67
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-04 08:32:37 +0000

    added empty test skeletons for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+    describe "creating User" do
+        it "should should call the model method that creates User" do
+            pending
+        end
+        it "should detect if User Id already exists" do
+            pending
+        end
+    end
 end
