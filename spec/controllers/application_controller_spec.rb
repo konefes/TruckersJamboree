99d333c1939709d07b15fd9def04cfd969a349fe
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-04 08:43:22 +0000

    added empty test skeletons for sessions and application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-
+    describe "setting current User" do
+        it "should call model method that finds User by session token" do
+            pending
+        end
+    end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-
+    describe "creating Session" do
+        it "should call model method that checks for empty fields" do
+            pending
+        end
+        it "should call model method that finds User by User Id" do
+            pending
+        end
+    end
 end
