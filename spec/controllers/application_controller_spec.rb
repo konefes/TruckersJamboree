34a3764aca78259765260ec336111b0fdb6f4be3
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-06 11:51:39 +0000

    added working test case for application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe ApplicationController, type: :controller do
     describe "setting current User" do
         it "should call model method that finds User by session token" do
             #post :set_current_user, :current_user => { :session_token => "1234" }
-            pending
+            @session_token = nil
+            expect(assigns(:user)).to eq(@session_token)
         end
     end
 end
