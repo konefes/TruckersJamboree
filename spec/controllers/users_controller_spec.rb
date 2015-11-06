f74b0089507b1346cbfc7b05df84ebf83afd6ac0
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-06 08:14:35 +0000

    added working test cases for users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
     describe "creating User" do
         it "should should call the model method that creates User" do
-            pending
+            post :create, :user => { :username => "testing123", :email => "testing@testing.com",
+                                     :password => "123123pasword!", :role => "admin" }
         end
         it "should detect if User Id already exists" do
-            pending
+            expect { post :create, :user => {:user => :username } }.to change{ User.count }
         end
     end
 end
