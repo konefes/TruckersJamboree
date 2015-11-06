c09f60f340ff6fbab344427e233a82155a4a2b72
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-06 11:46:34 +0000

    added working test cases for sessions controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
     describe "setting current User" do
         it "should call model method that finds User by session token" do
+            #post :set_current_user, :current_user => { :session_token => "1234" }
             pending
         end
     end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
     describe "creating Session" do
         it "should call model method that checks for empty fields" do
-            pending
+             expect(assigns(:user)).to eq(nil)
         end
         it "should call model method that finds User by User Id" do
-            pending
+            post :create, :user => { :username => "testing123" }
         end
     end
 end
