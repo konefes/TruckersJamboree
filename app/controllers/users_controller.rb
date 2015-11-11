0b93732171dd273dc26848ea5fc37033a6b029b5
muhammedyusuf_sermet <muhammedyusuf-sermet@uiowa.edu>
2015-11-11 23:55:48 +0000

    Validation method upgrade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,24 @@ class UsersController < ApplicationController
 
   def create
 
-    begin
-       @user=User.create_user!(user_params)
-       flash[:notice] = "New user #{@user.username} was successfully created."
-       redirect_to login_path
-    rescue ActiveRecord::RecordInvalid => e #This section is to resolve the same username conflict
-       flash[:notice] = "The User Id #{params[:user][:username]} already exists"
-       redirect_to new_user_path
-    end
+      message = ""
     
+      @user=User.new(user_params)
+      @user.valid?
+      if !@user.errors[:username].empty?
+        message+="User name has already been taken\n"
+      end
+      
+      
+      if message.empty?
+        @user.save
+        flash[:notice] = "New user #{@user.username} was successfully created."
+        redirect_to login_path
+      else
+       flash[:notice] = message
+       redirect_to new_user_path
+      end
+
   end
 
 
