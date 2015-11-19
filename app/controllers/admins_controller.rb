38c6a0b0dcf1f97ad1961dc3f31c8e01f206e3bb
konefes <konefes@gmail.com>
2015-11-19 19:52:47 +0000

    sorting functions started

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,4 +5,13 @@ class AdminsController < ApplicationController
       @vendors = Vendor.all 
       #test view in index. Will remove later
     end
+    
+    def populate_list
+      @vendors = sort_by_checkbox();
+      render :partial=>'vendor_list', :object=>@vendors and return if request.xhr?
+    end
+    
+    def print_list
+      @vendors = sort_by_checkbox();
+    end
 end
