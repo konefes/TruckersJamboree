1231ab4b12a76c6aeb9edd1e270ab9865a60f98d
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-11-23 21:21:35 +0000

    added required environments for smtp setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#tells ActionMailer that you want to use the SMTP server.
+config.action_mailer.delivery_method = :smtp 
+
+config.action_mailer.smtp_settings = {
+   address:              'smtp.live.com',
+   port:                 587,
+   domain:               'example.com',
+   user_name:            '<username>',
+   password:             '<password>',
+   authentication:       'plain',
+   enable_starttls_auto: true  
+}
+
+ActionMailer::Base.default_content_type = "text/html"
