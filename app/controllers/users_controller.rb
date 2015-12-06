8e80d1556c93b3d003e7cea5602517c8046c4cde
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-12-06 19:38:21 +0000

    adjusted email function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,9 +37,9 @@ class UsersController < ApplicationController
       
       
       if @user.save
-        #@user = User.create_user!(user_params)
         UserMailer.welcome_email(@user).deliver_now
-        flash[:notice] = "New user #{@user.username} was successfully created."
+        #@user = User.create_user(user_params) #generate random session token
+        flash[:notice] = "New user #{@user.username} was successfully created. Welcome email sent."
         redirect_to login_path
       else
        flash[:notice] = message
