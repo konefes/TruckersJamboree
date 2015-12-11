86058e469e51dd561c3e14fcf1cfcd4e41f99d46
bmreynolds <benjamin-reynolds@uiowa.edu>
2015-12-11 01:35:33 +0000

    added basic framework for vendor reg email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,12 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Truckers Jamboree!')
+  end
+  
+  def vendor_email(current)
+    @current = current
+    @url  = 'http://example.com/login'
+    mail(to: @current.email, subject: 'Vendor Registration Receipt!')
   end
 end
